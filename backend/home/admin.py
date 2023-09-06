810d31c7de883f23fa8d1021b66c09a8b1151564
crowdbotics-dev <engineering@crowdbotics.com>
2023-09-06 17:03:37 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Frte
+admin.site.register(Frte)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Frte(models.Model):
+    'Generated Model'
+    gfhfj = models.BigIntegerField()
